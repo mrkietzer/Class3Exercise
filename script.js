18267f5c2be2f9fa126bf347915d5d6f7c8229c9
mrkietzer <mrkietzer@gmail.com>
2023-08-28 20:12:01 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var leng = 8;
+var wid = 3;
+
+var area = leng * wid;
+
+console.log(area);
+
+var myNumber = 13;
+
+if (myNumber % 2 == 0) { console.log(myNumber, " number is even") }
+else { console.log(myNumber, "is odd") }
